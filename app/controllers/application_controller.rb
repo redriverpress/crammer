c9de74855596a827f2a51c69eed77f36a8617558
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-07-17 16:15:07 -0500

    Installed activeadmin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def welcome; end
+
+  protected
+
+  def access_denied(error)
+    redirect_to root_path
+  end
 end
