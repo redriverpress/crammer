75e0fe136ac7aa9cb047cbf9883d18026432a4a0
root <qian91328@gmail.com>
2017-07-24 06:25:39 +0000

    Test validations of Placement model

--- spec/models/crammer/placement_spec.rb
+++ spec/models/crammer/placement_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Crammer::Placement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with user and crammer_class" do
+    expect(Crammer::Placement.new).to be_valid
+  end
+
+  it "is not valid without a user"
+  it "is not valid without a crammer_class"
 end
